1a3d8a9b032bf9c9d28a0321f637f64f2589e2dc
sanjidtk <48551302+sanjidtk@users.noreply.github.com>
2020-07-18 12:33:36 +0530

    Update deleteAllMedia.js

--- src/deleteAllMedia.js
+++ src/deleteAllMedia.js
@@ -125,9 +125,8 @@ console.log(chalk`
 
   ——————————————————  [THANKS TO]  ————————————————————
   [✓] CODE BY CYBER SCREAMER CCOCOT (ccocot@bc0de.net)
-  [✓] FIXING & TESTING BY SYNTAX (@teamsyntaxid)
-  [✓] CCOCOT.CO | BC0DE.NET | NAONLAH.NET | WingkoColi
-  [✓] SGB TEAM REBORN | Zerobyte.id | ccocot@bc0de.net 
+  [✓] FIXING & TESTING BY SYNTAX (@officialputu_id)
+  [✓] MODIFIED BY @mohsanjid X PhotoLooz
   —————————————————————————————————————————————————————}
       `);
 
